67eef8f96d53f00e00b451a458ef43c70128d375
Flavio Bizzarri <33379291+newfla@users.noreply.github.com>
2026-08-28 19:17:19 +0200

    Set package ecosystem to 'pub' for Dependabot (#1)
    * Set package ecosystem to 'pub' for Dependabot
    
    * feat: add CI workflow for testing Android and iOS builds
    
    * feat: update build process and fix return types in generated entities

--- lib/core/providers/stream_providers.g.dart
+++ lib/core/providers/stream_providers.g.dart
@@ -574,7 +574,7 @@ abstract class _$FocusedMonth extends $Notifier<DateTime> {
   DateTime build();
   @$mustCallSuper
   @override
-  void runBuild() {
+  WhenComplete runBuild() {
     final ref = this.ref as $Ref<DateTime, DateTime>;
     final element =
         ref.element
@@ -584,7 +584,7 @@ abstract class _$FocusedMonth extends $Notifier<DateTime> {
               Object?,
               Object?
             >;
-    element.handleCreate(ref, build);
+    return element.handleCreate(ref, build);
   }
 }
 
@@ -644,7 +644,7 @@ abstract class _$TodayDateKey extends $Notifier<String> {
   String build();
   @$mustCallSuper
   @override
-  void runBuild() {
+  WhenComplete runBuild() {
     final ref = this.ref as $Ref<String, String>;
     final element =
         ref.element
@@ -654,7 +654,7 @@ abstract class _$TodayDateKey extends $Notifier<String> {
               Object?,
               Object?
             >;
-    element.handleCreate(ref, build);
+    return element.handleCreate(ref, build);
   }
 }
 
